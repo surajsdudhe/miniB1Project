2c35597d82d25d6629522291e0449fbe4bf99aed
netware <netware@DESKTOP-E9M0GDP>
2022-10-15 22:44:27 +0530

    test

--- MinitProjectB1/src/com/project_mini_1/Home.java
+++ MinitProjectB1/src/com/project_mini_1/Home.java
@@ -5,7 +5,13 @@ public class Home {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub  
-		System.out.println(" Enter 1  for log in");
+		System.out.println(" Enter 1  for log in");  
+		System.out.println(" Enter 2  for select product type in");  
+		System.out.println(" Enter 3  for Cart in");  
+		System.out.println(" Enter 4  for history in");  
+		System.out.println(" Enter 5  for log in");  
+		System.out.println(" Enter 6  for log in");  
+		System.out.println(" Enter 7  for log in");
 
 	}
 
