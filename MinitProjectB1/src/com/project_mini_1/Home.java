8a9a10ac9ccab767b48ce66e614b91b85dd6ad55
netware <netware@DESKTOP-E9M0GDP>
2022-10-15 22:48:20 +0530

    test2

--- MinitProjectB1/src/com/project_mini_1/Home.java
+++ MinitProjectB1/src/com/project_mini_1/Home.java
@@ -11,7 +11,7 @@ public class Home {
 		System.out.println(" Enter 4  for history in");  
 		System.out.println(" Enter 5  for log in");  
 		System.out.println(" Enter 6  for log in");  
-		System.out.println(" Enter 7  for log in");
+		System.out.println(" Enter 7  for log in"); 
 
 	}
 
