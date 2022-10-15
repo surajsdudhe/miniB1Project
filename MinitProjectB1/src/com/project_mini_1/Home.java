99720324d0e285fe597f58a21ca8dc6d976a7ddb
netware <netware@DESKTOP-E9M0GDP>
2022-10-15 22:18:29 +0530

    1st commit

---
+++ MinitProjectB1/src/com/project_mini_1/Home.java
@@ -0,0 +1,11 @@
+package com.project_mini_1;
+
+public class Home {
+// https://github.com/surajsdudhe/miniB1Project.git
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
